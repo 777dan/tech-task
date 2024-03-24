579764021f15388441a6b6943ca0f0348639105b
777dan <danya.nikitin7@gmail.com>
2024-03-24 18:47:44 +0200

    added drag and drop

---
+++ task3.js
@@ -0,0 +1,51 @@
+const blocks = document.querySelector("#blocks");
+const field = document.querySelector("#field");
+let numbers = [5, 1, 2, 4, 3];
+
+const genBlocks = () => {
+    for (let i = numbers.length - 1; i > 0; i--) {
+        let randNum = Math.floor(Math.random() * (i + 1));
+        let temp = numbers[i];
+        numbers[i] = numbers[randNum];
+        numbers[randNum] = temp;
+    }
+    for (let i = 1; i <= 5; i++) {
+        let block = `<div class='blocks' id='block${numbers[i - 1]}' draggable="true" ondragstart="drag(event)" data-id='${numbers[i - 1]}'>${numbers[i - 1]}</div>`;
+        blocks.innerHTML += (block);
+    }
+}
+genBlocks();
+
+function allowDrop(event) {
+    if (event.target.id === "field") {
+        event.preventDefault();
+    }
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
+
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener('click', () => {
+    let isOrderTrue = true;
+    for (let i = 0; i < field.children.length; i++) {
+        if (field.children[i].getAttribute('data-id') != i + 1) {
+            isOrderTrue = false;
+            break;
+        }
+    }
+    if (isOrderTrue) {
+        alert("OK");
+    } else {
+        alert("Error");
+    }
+});
+
